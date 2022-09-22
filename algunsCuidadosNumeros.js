c9e82acd074f29b63f23709b3a72d7d743ad20ce
Cavall <antoniodonizethe@hotmail.com>
2022-09-22 12:36:47 -0400

    Incluso arquivo algunsCuidadosNumeros

---
+++ algunsCuidadosNumeros.js
@@ -0,0 +1,6 @@
+console.log(77/0); // No JavaScript divise-se por zeero...resultado Infinity
+console.log("20"/2); //Dividindo algumas strings que podem ser convertidas pela linguagem
+console.log("Bola" * 4); // nao divide..retorna Nan
+console.log(0.1 + 0.7); //segue padrao proprio de arrendodamento
+//console.log(20.toString());//literal nao pode chamar funcao
+console.log((20.79875).toFixed(2)) //com parenteses eh possivel chamar com literal
