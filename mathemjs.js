2580b65c1d62a8e20209f66a8dd97b48526d1995
Cavall <antoniodonizethe@hotmail.com>
2022-09-22 12:54:52 -0400

    incluso arquivo mathemjs.js com objeto Math

---
+++ mathemjs.js
@@ -0,0 +1,16 @@
+console.log(`Math em Java Script eh um Object.`)
+
+console.log(`Ao contrário de outros objetos globais, Math não é um construtor. 
+Todas as propriedades e métodos de Math são estáticos. 
+Você pode referenciar a constante PI como Math.PI e você pode chamar a função de seno como Math.sin (x),
+onde x é o argumento do método. 
+Constantes são definidas com a precisão total de números reais em JavaScript.`);
+
+//Usando o objeto Math no Java Script
+
+const raio = 4.0;
+const area = Math.PI * Math.pow(raio, 2);
+console.log( area);
+console.log(`Math eh um: `, typeof Math);
+console.log("Number eh um: " , typeof Number);
+console.log("number eh um: " , typeof number);
