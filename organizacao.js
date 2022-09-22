f42cfba55969b316576e7257d16e67bd70aa64fa
Cavall <antoniodonizethe@hotmail.com>
2022-09-21 21:22:27 -0400

    incluso arquivo organizacao

---
+++ organizacao.js
@@ -0,0 +1,12 @@
+//javascript eh organizado por sentencas e blocos de codigos
+
+console.log("Sentenca de codigo");
+
+{ console.log("javascript eh organizado por sentencas e blocos de codigos");
+    {
+        console.log("Ola");
+        console.log('Mundo')
+
+    }
+
+}
