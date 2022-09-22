0a0a23d270c1f0d84f4ebed7e1e4998e8add7955
Cavall <antoniodonizethe@hotmail.com>
2022-09-22 19:56:19 -0400

    Incluso arquivo quasetudoehfuncao...Estudou function?

---
+++ quasetudoehfuncao.js
@@ -0,0 +1,11 @@
+console.log(`Funções são blocos de construção fundamentais em JavaScript. 
+Uma função é um procedimento de JavaScript - um conjunto de instruções que executa uma tarefa ou calcula um valor.
+Para usar uma função, você deve defini-la em algum lugar no escopo do qual você quiser chamá-la.`)
+
+console.log(`-----------------------------------------------------------------------------`)
+console.log(`Em Java Script quasse tudo eh funcao`);
+
+console.log('Object eh: ', typeof Object);
+
+class Produto {};
+console.log('Clase eh: ', typeof Produto);
