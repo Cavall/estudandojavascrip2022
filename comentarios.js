d8a1889fe8ad7dfa61022b90e7fe349acc440e51
Cavall <antoniodonizethe@hotmail.com>
2022-09-21 21:18:07 -0400

    comimit inicial com arquivo comentarios

---
+++ comentarios.js
@@ -0,0 +1,27 @@
+// Usamos //  para comentarios de uma unica linha
+console.log('Usamos //  para comentarios de uma unica linha.');
+
+console.log("-----------------------------------------------");
+
+/* Usamos /*  e o inverso desses dois caracteres  para comentarios
+de multiplas linhas */
+console.log(`Usamos /* e */ para comentarios cujo texto...
+...exijam multiplas linhas.`);
+
+console.log("-----------------------------------------------");
+
+/*Alugns programadores
+ * gostam de usar
+ * multiplas * nos comentarios de
+ * multiplas linhas
+ * 
+ * 
+ * 
+ * 
+ * */
+console.log(`Alugns programadores
+* gostam de usar
+* multiplos * nos comentarios de
+* multiplas linhas`);
+
+console.log("-----------------------------------------------");
