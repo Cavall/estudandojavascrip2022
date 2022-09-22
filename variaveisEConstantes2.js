07dc4b7aca48dac052872b0429c740464deaa32f
Cavall <antoniodonizethe@hotmail.com>
2022-09-22 10:58:25 -0400

    Incluso arquivo variaveisEConstanes2

---
+++ variaveisEConstantes2.js
@@ -0,0 +1,14 @@
+console.log(`De acordo com o mdn, existem tres tipos de declaracoes em JavaScript. Sao elas: var, let e const.`);
+
+console.log(`Com a palavra reservada var : Declaramos uma variável, opcionalmente, inicializando-a com um valor...veja exemplos`);
+var exemplo = `Exemplo de var`;
+console.log(exemplo);
+var exemplo1 = 7.78;
+console.log(exemplo1);
+
+console.log(`Com a palavra reservada let : Declaramos uma variável local de escopo de bloco, opcionalmente, inicializando-a com um valor...veja exemplos`);
+let exemploDeLet = `Exemplo de let`;
+let exemploDeLet2 = 74.57;
+
+console.log(`Com a palavra reservada const : Declaramos uma constante de escopo de bloco, apenas de leitura... veja exemplo`);
+const exemploDeConst = (`Exemplo de const com String`);
