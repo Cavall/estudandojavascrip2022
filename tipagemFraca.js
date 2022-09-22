cba53d1cff127e520922f2c9a3585ea8c253fcdf
Cavall <antoniodonizethe@hotmail.com>
2022-09-21 22:04:11 -0400

    incluso arquivo tipagem fraca

---
+++ tipagemFraca.js
@@ -0,0 +1,11 @@
+console.log(`No javascrip a tipagem eh dinamica. Com isso uma variavel, por exemplo,
+pode assumir tipos diferentes...vejamos exemplo abaixo:`)
+
+let qualquer = "Sofia Loren";
+console.log(qualquer, "....", typeof qualquer);
+
+//modificando o valor da variavel qualquer
+qualquer = 3.57;
+console.log(qualquer, "....", typeof qualquer);
+// Seja criterioso com os nomes das suas variaveis. 
+console.log(`Evite usar nomes genericos e desconexos para suas variaveis. Nao use valor, numero, siglas, etc...`);
