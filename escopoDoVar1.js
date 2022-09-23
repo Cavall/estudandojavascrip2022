862a66122e87f231941a7344c69c73a9c154438d
Cavall <antoniodonizethe@hotmail.com>
2022-09-23 09:57:21 -0400

    Adicionado escopoDoVar1 com var no bloco e na funcao  em 230922

---
+++ escopoDoVar1.js
@@ -0,0 +1,28 @@
+console.log(`O escopo de uma variavel declarada com var eh de funcao e global.`);
+
+
+//exemplo
+{
+    {
+        {
+            var cor = 'Amarelo';
+
+        }
+
+    }
+}
+
+
+
+
+function escreveCor() {
+    var cor;
+    console.log("dentro da funcao: ", cor); //aqui nao teremos saida da cor porque ela so foi declarada e nao teve atribuicao
+    //vale portanto o escopo de funcao
+}
+console.log('chamando a funcao fora da  funcao: ', escreveCor()); //resulta em undefined
+console.log(cor); // aqui e mantendo a declaracao de var na function sem atribuicao teremos Amarelo
+console.log(`Neste exemplo a variavel var cor declarada dentro de diversos blocos
+foi acessada porque ela tem escopo global e fora da funcao`);
+
+
