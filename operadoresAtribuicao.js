6fe60f2982fce556e2610ea2148ce039952a6712
Cavall <antoniodonizethe@hotmail.com>
2022-09-26 11:02:01 -0400

    Adicionado arquivo operadoresAtribuicao.js em 260922

---
+++ operadoresAtribuicao.js
@@ -0,0 +1,22 @@
+//Operadores de atribuicao
+
+console.log(`Um operador de atribuição atribui um valor ao operando à sua esquerda
+baseado no valor do operando à direita. O operador de atribuição básico é o igual (=),
+que atribui o valor do operando à direita ao operando à esquerda. Isto é, x = y atribui o valor de y a x.
+Os outros operadores de atribuição são encurtamentos de operadores padrão, como mostrado, por exemplo, a seguir:
+x += y...significa x = x+y`);
+
+const a = 79;
+let x = 33;
+console.log('x = ', x);
+x += 77;
+console.log('x = ', x);
+x -= 50;
+console.log('x = ', x);
+x /= 2;
+console.log('x = ', x);
+x %= x;
+console.log('x = ', x);
+
+
+console.log(`Existem outros operadores de atribuicao vide tabela mdn`)
