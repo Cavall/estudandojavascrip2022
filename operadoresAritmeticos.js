63fef4ddc862f505befae497af7da38784791ffd
Cavall <antoniodonizethe@hotmail.com>
2022-09-27 14:27:17 -0400

    Adicionado arquivo operadoresAritmetiocos.js em 270922

---
+++ operadoresAritmeticos.js
@@ -0,0 +1,14 @@
+console.log(`Operadores aritimeticos normalmente sao binarios:`)
+console.log(`Sao os mesmos operadores que utilizamos
+para realizar as operacoes da nossa matematica basica: +,-,*,/, e % :`)
+
+
+const [a, b, c, d, e, f] = [2,4,6,8,10, 9]
+const soma = a +b;
+const subtracao = e -f;
+const multiplicacao = c * d;
+const divisao = d / a;
+const resto = f % 2;
+
+
+console.log(soma, subtracao, multiplicacao, divisao, resto);
